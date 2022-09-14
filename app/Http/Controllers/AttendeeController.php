5f8e6d1514410577747900dda39ab44d11daef9b
jlacs <joselacsoniii@gmail.com>
2022-09-14 11:10:47 +0800

    Concert App for DQL Item 9

--- app/Http/Controllers/AttendeeController.php
+++ app/Http/Controllers/AttendeeController.php
@@ -4,20 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Entities\Attendee;
 use Illuminate\Http\Request;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 
 class AttendeeController extends Controller
 {
     private $em;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManager $em)
     {
         $this->em = $em;
     }
 
     public function index()
     {
-        $attendee = $this->em->getRepository(Attendee::class)->findAll();
+        $query_builder = $this->em->getRepository(Attendee::class)->createQueryBuilder('a');
+        $query_builder->where('a.id = 3');
+
+        $query = $query_builder->getQuery();
+        $attendee = $query->getResult();
 
         dd($attendee);
     }
